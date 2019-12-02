093cf7f25171b092a67ec0fb25cf33bd95e844e4
UniverseHero <897013045@qq.com>
2019-12-02 20:39:03 +0800

    增加根据账号进行查找的操作

--- src/main/java/top/arron206/whisper/dao/UserRepository.java
+++ src/main/java/top/arron206/whisper/dao/UserRepository.java
@@ -3,6 +3,8 @@ package top.arron206.whisper.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import top.arron206.whisper.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findById(int id);
+    User findByAccount(String account);
 }
