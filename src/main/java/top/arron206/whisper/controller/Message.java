fbc110e014b893879569d066616f66aaf453812d
UniverseHero <897013045@qq.com>
2019-12-05 09:30:52 +0800

    增加获取聊天记录的接口

---
+++ src/main/java/top/arron206/whisper/controller/Message.java
@@ -0,0 +1,52 @@
+package top.arron206.whisper.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import top.arron206.whisper.dao.RecordRepository;
+import top.arron206.whisper.dto.*;
+import top.arron206.whisper.entity.User;
+import top.arron206.whisper.service.*;
+import top.arron206.whisper.vo.*;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/verification/message")
+public class Message {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserMessage userMessage;
+
+    @Autowired
+    RecordRepository recordRepository;
+
+    @RequestMapping(value = "/index", method = {RequestMethod.GET})
+    public InfoMessage getIndex(){
+        User user = userService.setUser();
+        Map<Object, Object> unreadMessages = userMessage.getAllUnread(String.valueOf(user.getId()));
+        LinkMessage linkMessage = new LinkMessage(
+                "index /verification/message/index",
+                "/verification/message/index",
+                "index",
+                "application/json");
+        return new InfoMessage("获取成功", unreadMessages, new UserIndexDto(
+                user.getId(), user.getNickname(), user.getPic(), user.getGender(), user.getAge()
+        ), linkMessage);
+    }
+
+    @RequestMapping(value = "/history", method = {RequestMethod.GET})
+    public HistoryMessage getHistory(@RequestParam Integer customer, @RequestParam Integer page){
+        User user = userService.setUser();
+        userMessage.readMessage(String.valueOf(user.getId()), String.valueOf(customer));
+        List<RecordDto> records = recordRepository.getChatRecord(user.getId(), customer, page);
+        LinkMessage linkMessage = new LinkMessage(
+                "index /verification/message/index",
+                "/verification/message/index",
+                "index",
+                "application/json");
+        return new HistoryMessage("获取成功", records, linkMessage);
+    }
+}
