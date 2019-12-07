bd39d1d324990d25e706f6f52ee9af320b2c6627
傅诚 <327619852@qq.com>
2019-12-07 12:18:52 +0800

    whisper-fe init webpack project

---
+++ whisper-fe/webpack.base.conf.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.join(__dirname, '/dist')  //打包生成文件地址
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html')
+    }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['./dist']
+    }),
+    new VueLoaderPlugin()
+  ]
+};
