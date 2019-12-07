5cf2d6fe020c2176800da73bec62f03897aaf183
傅诚 <327619852@qq.com>
2019-12-07 16:59:32 +0800

    add mint-ui

--- whisper-fe/src/index.js
+++ whisper-fe/src/index.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 Vue.prototype.axios = axios
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
 
 const root = document.createElement('div') //创建div节点
 document.body.appendChild(root) //将div节点添加到body下
