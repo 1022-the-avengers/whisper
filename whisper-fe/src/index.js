d5d1ec39ab8511bdfb85b5c10f50b7b15e773672
傅诚 <327619852@qq.com>
2019-12-08 20:30:58 +0800

    add global function

--- whisper-fe/src/index.js
+++ whisper-fe/src/index.js
@@ -3,10 +3,57 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 Vue.prototype.axios = axios
+axios.defaults.baseURL = 'http://192.168.1.105' // 本地测试
+
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 Vue.use(MintUI)
 import './assets/iconfont/iconfont.css'
+
+axios.interceptors.request.use(config => {
+  // console.log("request config:  ", config)
+  return config
+}, err => {
+  this.$store.state.data = '请求超时！'
+  this.Popup(this.$store.state.data)
+  return Promise.resolve(err)
+  console.log(this.$store.state.data)
+})
+axios.interceptors.response.use(respone => {
+  // console.log("respone :", respone)
+  return respone
+}, err => {
+  if (err.respone.status === 500 || err.respone.status === 504 || err.respone.status === 404) {
+    this.$store.state.data = '服务器炸了⊙﹏⊙∥'
+    this.Popup(this.$store.state.data)
+    console.log("err respone", err.respone.status + this.$store.state.data)
+  } else {
+    this.$store.state.data = '服务发生错误!'
+    this.Popup(this.$store.state.data)
+  }
+  return Promise.resolve(err)
+})
+
+//全局注册，方便在所有组件调用
+//验证邮箱格式是否正确
+Vue.prototype.isEmail = function (str) {
+  let myReg = /^[-_A-Za-z0-9]+@([_A-Za-z0-9]+\.)+[A-Za-z0-9]{2,3}$/
+  if (myReg.test(str)) {
+    return true
+  } else {
+    return false
+  }
+}
+//控制Toast显隐
+Vue.prototype.Popup = function (msg) {
+  if (msg !== undefined) {
+    this.Msg = msg
+  }
+  this.visible = true
+  setTimeout(() => {
+    this.visible = false
+  }, 1000)
+}
 
 const root = document.createElement('div') //创建div节点
 document.body.appendChild(root) //将div节点添加到body下
