d2a2a9a7671874c2849e6cdd237823a6c875cb54
傅诚 <327619852@qq.com>
2019-12-08 16:52:18 +0800

    update scss

--- whisper-fe/webpack.base.conf.js
+++ whisper-fe/webpack.base.conf.js
@@ -21,8 +21,12 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.sass$/,
-        loaders: ['style', 'css', 'sass']
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 将 JS 字符串生成为 style 节点
+          "css-loader", // 将 CSS 转化成 CommonJS 模块
+          "sass-loader" // 将 Sass 编译成 CSS，默认使用 Node Sass
+        ]
       },
       {
         test: /\.js$/,
