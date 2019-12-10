5571b69a0210cefeb636a714fdc2ea8aff26a316
southrock <southrock@foxmail.com>
2019-12-10 22:32:20 +0800

    后端简单对接

--- whisper-fe/src/views/home/home.vue
+++ whisper-fe/src/views/home/home.vue
@@ -11,12 +11,14 @@
     </div>
     <mt-tab-container v-model="selected">
       <mt-tab-container-item id="messages-container">
-        <div class="message-list-item">
-          <img src="https://avatars3.githubusercontent.com/u/32596089?s=40&v=4" alt="头像" class="avatar-img">
-          <div class="message-infomation">
-            <span class="message-username">最可爱的xxx</span>
-            <span class="message-preview">想你</span>
-            <span class="message-unread">1</span>
+        <div v-if="unreadMessages">
+          <div class="message-list-item" v-for="(message,id) of unreadMessages" v-bind:key="id">
+            <img src="https://avatars3.githubusercontent.com/u/32596089?s=40&v=4" alt="头像" class="avatar-img">
+              <div class="message-infomation">
+                <span class="message-username">{{ message.nickname }}</span>
+                <span class="message-preview">{{ message.content }}</span>
+                <span class="message-unread">{{ unreadNum[id] }}</span>
+              </div>
           </div>
         </div>
       </mt-tab-container-item>
@@ -24,7 +26,7 @@
         朋友
       </mt-tab-container-item>
     </mt-tab-container>
-    <mt-tabbar v-model="selected" fixed=true>
+    <mt-tabbar v-model="selected" fixed>
         <mt-tab-item id="messages-container">
           消息列表
         </mt-tab-item>
@@ -41,11 +43,31 @@ export default {
     data() {
         return {
           searchValue: "",
-          selected: "messages-container"
+          selected: "messages-container",
+          unreadMessages: {},
+          unreadNum: {},
+          userMessage: {},
         }
     },
     methods: {
-      
+      handleGetMessages(res) {
+        if (res) {
+          const { data } = res;
+          const { unreadMessages,unreadNum, userMessage } = data;
+          this.unreadMessages = unreadMessages;
+          this.unreadNum = unreadNum;
+          this.userMessage = userMessage;
+          // console.log(this.messageData);
+        }
+      }
+    },
+    created: function() {
+      this.axios
+        .get("/verification/message/index")
+        .then(this.handleGetMessages)
+        .catch(err => {
+            console.log("获取失败err: ", err);
+        })
     }
 
 }
