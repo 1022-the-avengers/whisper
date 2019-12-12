fdefdd0ef933c70189e4a99cd6eb16a749061b79
傅诚 <327619852@qq.com>
2019-12-12 16:57:12 +0800

    feat(chat): add send message

--- whisper-fe/src/views/chat/chat.vue
+++ whisper-fe/src/views/chat/chat.vue
@@ -3,16 +3,16 @@
     <div class="header">这是哈士奇</div>
     <div class="content">
       
-      <div class="content_item">
+      <div class="content_item" v-for="(item, index) in myMessage" :key="index">
         <div class="msg">
-          <span class="info">这是发给某人的一条消息</span>
+          <span class="info">{{item}}</span>
         </div>
          <img class="avator" alt='你的头像' src="http://img.flura.cn/myAvatar.jpg"/>>
       </div>
     </div>
     <form class="footer">
-      <input type="text" class="footer_inpuit">
-      <button class="footer_button">发送</button>
+      <input type="text" class="footer_inpuit" v-model="inputMessage">
+      <button class="footer_button"  v-on:click.prevent="sendMessage">发送</button>
     </form>
   </div>
 </template>
@@ -21,8 +21,18 @@
 export default {
   name: "chat",
   data() {
-    return {};
-  }
+    return {
+      inputMessage: '',
+      myMessage: ['这是发给某人的一条消息', '这是另一个']
+    };
+  },
+  methods: {
+    sendMessage() {
+      console.log('发送')
+      this.myMessage.push(this.inputMessage)
+      this.inputMessage = ''
+    }
+  },
 };
 </script>
 
