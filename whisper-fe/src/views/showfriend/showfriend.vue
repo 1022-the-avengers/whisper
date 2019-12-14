068a22d61c97270e6c3117f5d2431009a6cbad01
yiweiting <2638088319@qq.com>
2019-12-14 21:53:43 +0800

    标签添加，移动分组

--- whisper-fe/src/views/showfriend/showfriend.vue
+++ whisper-fe/src/views/showfriend/showfriend.vue
@@ -19,7 +19,12 @@
       <div class="tag">
         <div class="tag-title">印象</div>
         <div class="tag-items">
-          <div class="tag-item" v-for="item in info.impressions" :key="item.key">{{item}}</div>
+          <div
+            class="tag-item"
+            v-for="(value,key) in info.impressions"
+            :key="key"
+            @click="removeTag(key)"
+          >{{value}}</div>
         </div>
       </div>
       <mt-button size="large" type="primary">发送消息</mt-button>
@@ -43,31 +48,11 @@ export default {
         },
         {
           name: "移动好友",
-          method: function() {
-            MessageBox.prompt(" ", "分组管理", {
-              inputPlaceholder: "输入分组的名字"
-            })
-              .then(({ value, action }) => {
-                console.log(value);
-              })
-              .catch(err => {
-                // console.log("取消移动");
-              });
-          }
+          method: this.moveFriend
         },
         {
           name: "添加标签",
-          method: function() {
-            MessageBox.prompt(" ", "添加印象", {
-              inputPlaceholder: "输入印象的名字"
-            })
-              .then(({ value, action }) => {
-                console.log(value);
-              })
-              .catch(err => {
-                // console.log("取消标签");
-              });
-          }
+          method: this.addTag
         }
       ]
     };
@@ -77,17 +62,82 @@ export default {
       this.sheetVisible = true;
     },
     deleteFriend() {
-      var param = {customId:this.info.id};
+      var param = { customId: this.info.id };
       MessageBox.confirm("此操作将删除好友")
         .then(action => {
           this.axios
-            .delete("/verification/user/relationship",{params:param})
+            .delete("/verification/user/relationship", { params: param })
             .then(response => {
               if (response.data.message == "好友关系解除成功") {
                 MessageBox.alert("好友关系解除成功").then(action => {
                   this.$router.push({ path: "/main" });
                 });
               }
+            })
+            .catch(err => {
+              console.log(err); //异常
+            });
+        })
+        .catch(err => {
+          // console.log("删除取消");
+        });
+    },
+    addTag() {
+      MessageBox.prompt(" ", "添加印象", {
+        inputPlaceholder: "输入印象的名字"
+      })
+        .then(({ value, action }) => {
+          var add = {};
+          this.info.impressions.push(value);
+          add.targetId = this.info.id;
+          add.contents = this.info.impressions;
+          console.log(add);
+          this.axios
+            .post("/verification/user/impression", add)
+            .then(response => {
+              console.log(response.data);
+            })
+            .catch(err => {
+              console.log(err); //异常
+            });
+        })
+        .catch(err => {
+          // console.log("取消标签");
+        });
+    },
+    moveFriend() {
+      MessageBox.prompt(" ", "移动好友", {
+        inputPlaceholder: "输入移动至的分组名字"
+      })
+        .then(({ value, action }) => {
+          var move = {};
+          move.customId = this.info.id;
+          move.groupName = value;
+          console.log(move);
+          this.axios
+            .post("/verification/user/relationship", move)
+            .then(response => {
+              console.log(response.data);
+            })
+            .catch(err => {
+              console.log(err); //异常
+            });
+        })
+        .catch(err => {
+          // console.log("取消标签");
+        });
+    },
+    removeTag(key) {
+      MessageBox.confirm("此操作将删除标签")
+        .then(action => {
+          this.info.impressions.splice(key, 1);
+          var add = {};
+          add.targetId = this.info.id;
+          add.contents = this.info.impressions;
+          this.axios
+            .post("/verification/user/impression", add)
+            .then(response => {
+              console.log(response.data);
             })
             .catch(err => {
               console.log(err); //异常
