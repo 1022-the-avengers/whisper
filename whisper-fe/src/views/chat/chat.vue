2d1167381b48ecee38612445a61130df40e4f568
傅诚 <327619852@qq.com>
2019-12-15 10:44:39 +0800

    feat(connection): websocket connection

--- whisper-fe/src/views/chat/chat.vue
+++ whisper-fe/src/views/chat/chat.vue
@@ -58,19 +58,6 @@ export default {
     };
   },
   methods: {
-    initWebSocket() {
-      this.connection();
-      let self = this;
-      // 断开重连机制,尝试发送消息,捕获异常发生时重连
-      this.timer = setInterval(() => {
-        try {
-          self.stompClient.send("test");
-        } catch (err) {
-          console.log("断线了: " + err);
-          self.connection();
-        }
-      }, 5000);
-    },
     sendMessage() {
       console.log("发送");
       if (this.inputMessage) {
@@ -87,44 +74,6 @@ export default {
         );
         console.log("inputMessage: ", this.inputMessage);
         this.inputMessage = "";
-      }
-    },
-    connection() {
-      // 建立连接对象
-      let scoket = new this.SockJS("http://192.168.1.105:8080/chat"); //连接服务端提供的通信接口，连接以后才可以订阅广播消息和个人消息
-      // 获取STOMP子协议的客户端对象
-      this.stompClient = this.Stomp.over(scoket);
-      // 定义客户端的认证信息,按需求配置
-      var headers = {
-        // additional header
-        userId: 3
-      };
-      // 向服务器发起websocket连接
-      this.stompClient.connect(
-        headers,
-        frame => {
-          this.stompClient.subscribe("/subscription/3", msg => {
-            console.log("返回信息: ", msg);
-            console.log(JSON.parse(msg.body));
-            if (
-              JSON.parse(msg.body).content.he !== "发送成功" &&
-              JSON.parse(msg.body).content.he !== undefined
-            ) {
-              this.Messages.push(JSON.parse(msg.body).content);
-            }
-            // 订阅服务端提供的某个topic  // msg.body存放的是服务端发送给我们的信息
-          });
-        },
-        err => {
-          // 连接发生错误时的处理函数
-          console.log(err);
-        }
-      );
-    },
-    disconnect() {
-      if (this.stompClient != null) {
-        this.stompClient.disconnect();
-        console.log("Disconnected");
       }
     },
     getHistory() {
@@ -156,15 +105,10 @@ export default {
     }
   },
   mounted() {
-    this.initWebSocket();
     // 请求聊天记录
     this.getHistory();
   },
-  beforeDestroy() {
-    // 页面离开时断开连接,清除定时器
-    this.disconnect();
-    clearInterval(this.timer);
-  }
+
 };
 </script>
 
